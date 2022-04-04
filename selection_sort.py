bd624c488dd592647726942e4067d13f914c7f98
Alireza Raei <alirezaraei17@gmail.com>
2022-04-04 13:07:26 +1000

    selection_sort added.

---
+++ selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+print(selection_sort([4, 7, 1, 12, 9, 3, 5]))
