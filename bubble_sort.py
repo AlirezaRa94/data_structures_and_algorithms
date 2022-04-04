df304f7302f5a4002518e3bb040f0104fe403af0
Alireza Raei <alirezaraei17@gmail.com>
2022-04-04 13:21:28 +1000

    bubble sort added

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+print(bubble_sort([4, 7, 1, 12, 9, 3, 5]))
