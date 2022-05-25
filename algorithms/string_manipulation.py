eaf8601d75d8e19611e1abe10f3c49f3ccc1bf2e
Alireza Raei <alirezaraei17@gmail.com>
2022-05-25 17:02:23 +1000

    Added other methods to string manipulation.

--- algorithms/string_manipulation.py
+++ algorithms/string_manipulation.py
@@ -1,33 +1,29 @@
 """
 Some String Manipulation Algorithms
 """
-from collections import deque
+import string
 
 
 class StringManipulation:
     @staticmethod
-    def get_number_of_vowels(string: str):
-        if string is None:
-            return 0
+    def get_number_of_vowels(s: str):
         vowels = {'a', 'e', 'i', 'o', 'u'}
         counter = 0
-        for char in string:
+        for char in s:
             if char.lower() in vowels:
                 counter += 1
         return counter
 
     @staticmethod
-    def reverse(string: str):
-        if string is None:
-            return ""
+    def reverse(s: str):
         reversed_string = list()
-        for i in range(len(string)):
-            reversed_string.append(string[len(string) - 1 - i])
+        for i in range(len(s)):
+            reversed_string.append(s[len(s) - 1 - i])
         return "".join(reversed_string)
 
     @staticmethod
-    def reverse_word_orders(string: str):
-        words = string.strip().split()
+    def reverse_word_orders(s: str):
+        words = s.strip().split()
         reversed_order = list()
         i = len(words)
         while i > 0:
@@ -37,8 +33,6 @@ class StringManipulation:
 
     @staticmethod
     def is_rotation(s1: str, s2: str):
-        if s1 is None or s2 is None:
-            return False
         s1 = s1.strip()
         s2 = s2.strip()
         return len(s1) == len(s2) and s2 in s1 + s1
@@ -51,3 +45,67 @@ class StringManipulation:
             if s2[i] == s1[0] and s2[i:] + s2[:i] == s1:
                 return True
         return s1 == s2
+
+    @staticmethod
+    def remove_duplicates(s: str):
+        seen = set()
+        output = list()
+        for char in s.strip():
+            if char not in seen:
+                seen.add(char)
+                output.append(char)
+        return "".join(output)
+
+    @staticmethod
+    def get_most_repeated_character(s: str):
+        if not s:
+            raise ValueError("The string doesn't have any characters!")
+        char_counts = dict()
+        most_repeated = ""
+        max_repeat = 0
+        for char in s:
+            char_counts[char] = char_counts.get(char, 0) + 1
+            if char_counts[char] > max_repeat:
+                most_repeated = char
+                max_repeat = char_counts[char]
+        return most_repeated
+
+    @staticmethod
+    def capitalize_first_letters(s: str):
+        words = s.strip().split()
+        for i in range(len(words)):
+            first_letter = words[i][0]
+            if first_letter in string.ascii_letters:
+                words[i] = first_letter.upper() + words[i][1:].lower()
+        return " ".join(words)
+
+    @staticmethod
+    def is_anagram(s1: str, s2: str):
+        if len(s1) != len(s2):
+            return False
+        s1 = s1.replace(" ", "").lower()
+        s2 = s2.replace(" ", "").lower()
+        letter_count = dict()
+        for letter in s1:
+            letter_count[letter] = letter_count.get(letter, 0) + 1
+        for letter in s2:
+            count = letter_count.get(letter, 0)
+            if count < 1:
+                return False
+            letter_count[letter] = count - 1
+        return True
+
+    @staticmethod
+    def is_palindrome(s: str):
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            while not s[i].isalnum() and i < j:
+                i += 1
+            while not s[j].isalnum() and i < j:
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
--- tests/test_string_manipulation.py
+++ tests/test_string_manipulation.py
@@ -5,25 +5,29 @@ from algorithms.string_manipulation import StringManipulation
 
 class TestStringManipulation(TestCase):
     def setUp(self) -> None:
-        self.long_string = """  Hi! This is a long string for testing some algorithms of string manipulation. I have to
-         test everything. So this text should contain multiple numbers like: 1, 2, 5.  """
+        self.long_string = """  Hi! This is a   long string for testing some
+         algorithms of string manipulation. I have   to test everything. So this
+         text should contain multiple numbers like: 1, 2, 5.  """
         self.empty_string = ""
-        self.one_word = "Alireza"
+        self.one_word = "Alirezaa"
 
     def test_get_number_of_vowels(self):
-        self.assertEqual(45, StringManipulation.get_number_of_vowels(self.long_string))
-        self.assertEqual(0, StringManipulation.get_number_of_vowels(self.empty_string))
-        self.assertEqual(4, StringManipulation.get_number_of_vowels(self.one_word))
+        function = StringManipulation.get_number_of_vowels
+        self.assertEqual(45, function(self.long_string))
+        self.assertEqual(0, function(self.empty_string))
+        self.assertEqual(5, function(self.one_word))
 
     def test_reverse(self):
-        self.assertEqual(self.long_string[::-1], StringManipulation.reverse(self.long_string))
-        self.assertEqual("", StringManipulation.reverse(self.empty_string))
-        self.assertEqual(self.one_word[::-1], StringManipulation.reverse(self.one_word))
+        function = StringManipulation.reverse
+        self.assertEqual(self.long_string[::-1], function(self.long_string))
+        self.assertEqual("", function(self.empty_string))
+        self.assertEqual(self.one_word[::-1], function(self.one_word))
 
     def test_reverse_word_orders(self):
-        print(StringManipulation.reverse_word_orders(self.long_string))
-        self.assertEqual(self.empty_string, StringManipulation.reverse_word_orders(self.empty_string))
-        self.assertEqual(self.one_word, StringManipulation.reverse_word_orders(self.one_word))
+        function = StringManipulation.reverse_word_orders
+        print(function(self.long_string))
+        self.assertEqual(self.empty_string, function(self.empty_string))
+        self.assertEqual(self.one_word, function(self.one_word))
 
     def base_test_is_rotation(self, function):
         self.assertTrue(function("ABCDE", "BCDEA"))
@@ -38,3 +42,47 @@ class TestStringManipulation(TestCase):
 
     def test_is_rotation_2(self):
         self.base_test_is_rotation(StringManipulation.is_rotation_2)
+
+    def test_remove_duplicates(self):
+        function = StringManipulation.remove_duplicates
+        self.assertEqual(
+            'Hi! Thsalongtrfem\npu.IvySxdcbk:1,25',
+            function(self.long_string)
+        )
+        self.assertEqual(self.empty_string, function(self.empty_string))
+        self.assertEqual('Alireza', function(self.one_word))
+
+    def test_get_most_repeated_character(self):
+        function = StringManipulation.get_most_repeated_character
+        self.assertEqual(" ", function(self.long_string))
+        with self.assertRaises(ValueError) as error:
+            function(self.empty_string)
+            self.assertEqual(
+                error.exception,
+                "The string doesn't have any characters!"
+            )
+        self.assertEqual("a", function(self.one_word))
+
+    def test_capitalize_first_letters(self):
+        function = StringManipulation.capitalize_first_letters
+        print(function(self.long_string))
+        self.assertEqual("", function(self.empty_string))
+        self.assertEqual(self.one_word, function(self.one_word))
+
+    def test_is_anagram(self):
+        function = StringManipulation.is_anagram
+        self.assertTrue(function("ABCD", "BCDA"))
+        self.assertFalse(function("ABCD", "ABCE"))
+        self.assertTrue(function("ABCD", "ABCD"))
+        self.assertTrue(function("", ""))
+        self.assertFalse(function("", "B"))
+        self.assertTrue(function("A", "A"))
+
+    def test_is_palindrome(self):
+        function = StringManipulation.is_palindrome
+        self.assertTrue(function("ABbA"))
+        self.assertFalse(function("ABC"))
+        self.assertTrue(function("Was it a cat I saw?"))
+        self.assertTrue(function(""))
+        self.assertFalse(function("Ab"))
+        self.assertTrue(function("A!"))
