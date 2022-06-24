c32fa94e83ec3c6624448a3bc068a837c9e6a525
Alireza Raei <alirezaraei17@gmail.com>
2022-06-24 12:22:43 +1000

    queue: using a pointer for front of the queue.

--- data_structures/queues/queue.py
+++ data_structures/queues/queue.py
@@ -6,6 +6,7 @@ Queue Data Structure.
 class Queue:
     def __init__(self):
         self.items = list()
+        self.front = 0
 
     def __len__(self):
         return len(self.items)
@@ -14,12 +15,14 @@ class Queue:
         return len(self.items) == 0
 
     def enqueue(self, item):
-        self.items.insert(0, item)
+        self.items.append(item)
 
     def dequeue(self):
         if not self.is_empty():
-            return self.items.pop()
+            first_item = self.items[self.front]
+            self.front += 1
+            return first_item
 
     def peek(self):
         if not self.is_empty():
-            return self.items[-1]
+            return self.items[self.front]
